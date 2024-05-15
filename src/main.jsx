25fb7cf6ab0252d19f0757c68b4acc0b28969371
hannahnier <hannahr@posteo.de>
2024-05-15 16:11:49 +0200

    remove app.jsx, change swap-url to /adopt

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+import Router from "../src/utils/Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Router />
   </React.StrictMode>
 );
